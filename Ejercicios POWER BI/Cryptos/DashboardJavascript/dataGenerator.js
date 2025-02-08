b70403fa04099b4ccebeab2ab6de04ef48d50d3b
Agustín Palombo <agpalombo99@gmail.com>
2025-02-08 18:53:42 -0300

    Cryptos + Financial

--- Ejercicios POWER BI/Cryptos/DashboardJavascript/dataGenerator.js
+++ Ejercicios POWER BI/Cryptos/DashboardJavascript/dataGenerator.js
@@ -8,39 +8,39 @@ const globalUrl = 'https://api.coinpaprika.com/v1/global';
 
 async function fetchAndSaveData() {
   try {
-    // 1. Obtener datos de criptomonedas (Tickers)
-    const tickersResponse = await axios.get(tickersUrl);
-    const allTickers = tickersResponse.data;
+    // // 1. Obtener datos de criptomonedas (Tickers)
+    // const tickersResponse = await axios.get(tickersUrl);
+    // const allTickers = tickersResponse.data;
     
-    // Filtrar los 100 primeros (normalmente los de mayor ranking)
-    const top100Tickers = allTickers.slice(0, 100);
+    // // Filtrar los 100 primeros (normalmente los de mayor ranking)
+    // const top100Tickers = allTickers.slice(0, 100);
     
-    // Mapear los campos esenciales de cada criptomoneda
-    const filteredCurrentData = top100Tickers.map(coin => ({
-      id: coin.id,
-      name: coin.name,
-      symbol: coin.symbol,
-      rank: coin.rank,
-      max_supply: coin.max_supply ? String(coin.max_supply) : null, // Evita notación científica
-      total_supply: coin.total_supply ? String(coin.total_supply) : null,
-      beta_value: coin.beta_value.toFixed(0),
-      market_cap: String(coin.quotes.USD.market_cap), // Números grandes como string
-      price_change_24h: coin.quotes.USD.percent_change_24h.toFixed(2),
-      current_price: coin.quotes.USD.price.toFixed(2)
-    }));
+    // // Mapear los campos esenciales de cada criptomoneda
+    // const filteredCurrentData = top100Tickers.map(coin => ({
+    //   id: coin.id,
+    //   name: coin.name,
+    //   symbol: coin.symbol,
+    //   rank: coin.rank,
+    //   max_supply: coin.max_supply ? String(coin.max_supply) : null, // Evita notación científica
+    //   total_supply: coin.total_supply ? String(coin.total_supply) : null,
+    //   beta_value: coin.beta_value.toFixed(0),
+    //   market_cap: String(coin.quotes.USD.market_cap), // Números grandes como string
+    //   price_change_24h: coin.quotes.USD.percent_change_24h.toFixed(2),
+    //   current_price: coin.quotes.USD.price.toFixed(2)
+    // }));
     
-    // Convertir los datos de criptomonedas a CSV
-    const csvCoins = parse(filteredCurrentData, {
-      fields: [
-        'id', 'name', 'symbol','rank',
-        'max_supply', 'total_supply','beta_value',
-        'market_cap', 'price_change_24h','current_price'
-      ]
-    });
+    // // Convertir los datos de criptomonedas a CSV
+    // const csvCoins = parse(filteredCurrentData, {
+    //   fields: [
+    //     'id', 'name', 'symbol','rank',
+    //     'max_supply', 'total_supply','beta_value',
+    //     'market_cap', 'price_change_24h','current_price'
+    //   ]
+    // });
     
-    // Guardar el archivo CSV para las criptomonedas
-    fs.writeFileSync('crypto_data_coinpaprika.csv', csvCoins);
-    console.log('Archivo crypto_data_coinpaprika.csv generado correctamente!');
+    // // Guardar el archivo CSV para las criptomonedas
+    // fs.writeFileSync('crypto_data_coinpaprika.csv', csvCoins);
+    // console.log('Archivo crypto_data_coinpaprika.csv generado correctamente!');
     
     // 2. Obtener datos globales
     const globalResponse = await axios.get(globalUrl);
@@ -58,7 +58,7 @@ async function fetchAndSaveData() {
       volume_24h_ath_date: globalData.volume_24h_ath_date,
       market_cap_change_24h: globalData.market_cap_change_24h.toFixed(4),
       volume_24h_change_24h: globalData.volume_24h_change_24h.toFixed(4),
-      last_updated: globalData.last_updated
+      last_updated: new Date(globalData.last_updated)
 };
 
       const globalDataArray = [formattedGlobalData];
